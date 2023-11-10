6ad4b73ed003678119e2c0b3a920a4dc165b64d7
Utilisateur <Utilisateur@CAMPUS13-CDA>
2023-11-10 15:13:28 +0100

    Ajoutez des fichiers projet.

---
+++ ex9 1.2 modules/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9_1._2_modules
+{
+    class Program // trouver une valeur absolue
+    {
+        const int zero = 0; // Constante pour éviter valeurs dures
+        static int abs(int n) // fonction du cours à mod en procedure après
+        {
+
+            if (n < zero)
+            {
+                return -n;
+            }
+            else
+            {
+                return n;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int val = zero;
+            bool valide = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Entrer un chiffre = ");
+                    val = int.Parse(Console.ReadLine());
+                    valide = true;
+                    Console.WriteLine("");
+                }
+                catch
+                {
+                    Console.WriteLine("^ Erreur, entrer un entier ^");
+                }
+            } while (!valide);
+            Console.Write("La valeur absolue de " + val + " = " + abs(val));
+            Console.Read();
+        }
+    }
+}
